6dd0d04e1c632f63a2115f4731adecdfa11c17b0
ldurback <luke.durback@gmail.com>
2015-02-10 18:35:13 -0500

    reinitialized project to use dataset instead of database

--- Karaoke/Karaoke/Properties/AssemblyInfo.cs
+++ Karaoke/Karaoke/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Karaoke")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Karaoke")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("dd6514e9-19fe-4d59-b0e7-a206b5ec6c57")]
+[assembly: Guid("6d286729-5b86-4288-be94-d7516a3dcbc7")]
 
 // Version information for an assembly consists of the following four values:
 //
