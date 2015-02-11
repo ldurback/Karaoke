f059cc7b937ce553e36a1e7258ac503bc70e8861
Kohouri <kohouri@gmail.com>
2015-02-10 21:36:58 -0500

    Added UI elements in addition to ability to play the Mp3 files.
    // To do, make it display the graphics as well as only allow the MP3+G format files. As well as have the timer work appropriately.

--- Karaoke/KaraokeGUI/MainWindow.xaml.cs
+++ Karaoke/KaraokeGUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Karaoke
 {
@@ -23,6 +25,48 @@ namespace Karaoke
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            MediaPlayer.Play();
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            MediaPlayer.Pause();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            MediaPlayer.Stop(); 
+        }
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.AddExtension = true;
+            ofd.DefaultExt = "*.*";
+            ofd.Filter = "Media(*.*)|*.*";
+            ofd.ShowDialog();
+            MediaPlayer.MediaOpened += new RoutedEventHandler(MediaPlayer_MediaOpened);
+            MediaPlayer.Source = new Uri(ofd.FileName);
+        }
+
+
+        void MediaPlayer_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            SongText.Text = MediaPlayer.Source.ToString();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Start();
+
+            if (MediaPlayer.Source != null)
+                lblTimer.Content = String.Format("{0} / {1}", MediaPlayer.Position.ToString(@"mm\:ss"), MediaPlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
+            else
+                lblTimer.Content = "No file selected...";
+
         }
     }
 }
