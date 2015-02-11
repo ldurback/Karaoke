52e1d86027ddac2789419d5bd460d49807b268ad
ldurback <luke.durback@gmail.com>
2015-02-10 19:55:57 -0500

    Changed to uisng WPF

--- Karaoke/KaraokeParser/MP3DataSet.Designer.cs
+++ Karaoke/KaraokeParser/MP3DataSet.Designer.cs
@@ -10,7 +10,7 @@
 
 #pragma warning disable 1591
 
-namespace Karaoke {
+namespace KaraokeParser {
     
     
     /// <summary>
@@ -20,15 +20,15 @@ namespace Karaoke {
     [global::System.ComponentModel.DesignerCategoryAttribute("code")]
     [global::System.ComponentModel.ToolboxItem(true)]
     [global::System.Xml.Serialization.XmlSchemaProviderAttribute("GetTypedDataSetSchema")]
-    [global::System.Xml.Serialization.XmlRootAttribute("KaraokeDataSet")]
+    [global::System.Xml.Serialization.XmlRootAttribute("MP3DataSet")]
     [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.DataSet")]
-    public partial class KaraokeDataSet : global::System.Data.DataSet {
+    public partial class MP3DataSet : global::System.Data.DataSet {
         
         private global::System.Data.SchemaSerializationMode _schemaSerializationMode = global::System.Data.SchemaSerializationMode.IncludeSchema;
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
-        public KaraokeDataSet() {
+        public MP3DataSet() {
             this.BeginInit();
             this.InitClass();
             global::System.ComponentModel.CollectionChangeEventHandler schemaChangedHandler = new global::System.ComponentModel.CollectionChangeEventHandler(this.SchemaChanged);
@@ -39,7 +39,7 @@ namespace Karaoke {
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
-        protected KaraokeDataSet(global::System.Runtime.Serialization.SerializationInfo info, global::System.Runtime.Serialization.StreamingContext context) : 
+        protected MP3DataSet(global::System.Runtime.Serialization.SerializationInfo info, global::System.Runtime.Serialization.StreamingContext context) : 
                 base(info, context, false) {
             if ((this.IsBinarySerialized(info, context) == true)) {
                 this.InitVars(false);
@@ -112,7 +112,7 @@ namespace Karaoke {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
         public override global::System.Data.DataSet Clone() {
-            KaraokeDataSet cln = ((KaraokeDataSet)(base.Clone()));
+            MP3DataSet cln = ((MP3DataSet)(base.Clone()));
             cln.InitVars();
             cln.SchemaSerializationMode = this.SchemaSerializationMode;
             return cln;
@@ -175,9 +175,9 @@ namespace Karaoke {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
         private void InitClass() {
-            this.DataSetName = "KaraokeDataSet";
+            this.DataSetName = "MP3DataSet";
             this.Prefix = "";
-            this.Namespace = "http://tempuri.org/KaraokeDataSet.xsd";
+            this.Namespace = "http://tempuri.org/MP3DataSet.xsd";
             this.EnforceConstraints = true;
             this.SchemaSerializationMode = global::System.Data.SchemaSerializationMode.IncludeSchema;
         }
@@ -193,7 +193,7 @@ namespace Karaoke {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
         public static global::System.Xml.Schema.XmlSchemaComplexType GetTypedDataSetSchema(global::System.Xml.Schema.XmlSchemaSet xs) {
-            KaraokeDataSet ds = new KaraokeDataSet();
+            MP3DataSet ds = new MP3DataSet();
             global::System.Xml.Schema.XmlSchemaComplexType type = new global::System.Xml.Schema.XmlSchemaComplexType();
             global::System.Xml.Schema.XmlSchemaSequence sequence = new global::System.Xml.Schema.XmlSchemaSequence();
             global::System.Xml.Schema.XmlSchemaAny any = new global::System.Xml.Schema.XmlSchemaAny();
---
+++ Karaoke/KaraokeParser/Main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using System.Diagnostics;
+
+namespace MP3Parser
+{
+    public class Main
+    {
+        public void ParseDirectoryRecursively(string rootDirectory)
+        {
+            foreach (string path in Directory.EnumerateFiles(rootDirectory, "*.mp3", SearchOption.AllDirectories))
+            {
+                TagLib.Mpeg.File tagFile = null;
+
+                try
+                {
+                    tagFile = (TagLib.Mpeg.File)TagLib.File.Create(path);
+
+                    if (tagFile == null)
+                        throw new Exception("Opening file returned null");
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(path + " cannot be opened.  Ignoring.  Exception had message: " + e.Message);
+                    continue;
+                }
+            }
+        }
+    }
+}
