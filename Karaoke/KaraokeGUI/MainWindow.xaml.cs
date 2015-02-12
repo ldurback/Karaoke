0a53d472905cc6d87336e707440bb72c32a90ffc
Kohouri <kohouri@gmail.com>
2015-02-12 12:34:13 -0800

    Changed UI to be a bit more standard. Added text to explain how to run the program where the file location will be displayed.

--- Karaoke/KaraokeGUI/MainWindow.xaml.cs
+++ Karaoke/KaraokeGUI/MainWindow.xaml.cs
@@ -17,21 +17,24 @@ using System.Windows.Threading;
 
 namespace Karaoke
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+	/// <summary>Interaction logic for MainWindow.xaml</summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+
         public MainWindow()
         {
             InitializeComponent();
+            // Register your event watchers here to avoid calling the functions
+            // mutliple times for the same event trigger
+            MediaPlayer.MediaFailed += MediaPlayer_MediaFailed;
+            MediaPlayer.MediaOpened += new RoutedEventHandler(MediaPlayer_MediaOpened);
+            timer.Tick += timer_Tick;
         }
 
         private void PlayButton_Click(object sender, RoutedEventArgs e)
         {
-                MediaPlayer.MediaFailed += MediaPlayer_MediaFailed;
-                MediaPlayer.Play();
-                
+            MediaPlayer.Play();
         }
 
         private void PauseButton_Click(object sender, RoutedEventArgs e)
@@ -46,34 +49,26 @@ namespace Karaoke
 
         private void BrowseButton_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
+            var ofd = new OpenFileDialog();
             ofd.AddExtension = true;
             ofd.DefaultExt = "*.mp3";
             ofd.Filter = "Media|*.mp4;*.avi;*.flv;*.mp3";
             ofd.ShowDialog();
-            MediaPlayer.MediaOpened += new RoutedEventHandler(MediaPlayer_MediaOpened);
             MediaPlayer.Source = new Uri(ofd.FileName);
         }
-
 
         void MediaPlayer_MediaOpened(object sender, RoutedEventArgs e)
         {
             SongText.Text = MediaPlayer.Source.ToString();
-
-            DispatcherTimer timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(1);
-            timer.Tick += timer_Tick;
             timer.Start();
             lblTimer.Content = String.Format("{0} / {1}", MediaPlayer.Position.ToString(@"mm\:ss"), MediaPlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
         }
 
         void timer_Tick(object sender, EventArgs e)
         {
-            if (MediaPlayer.Source != null)
-            {
-                if (MediaPlayer.NaturalDuration.HasTimeSpan)
-                    lblTimer.Content = String.Format("{0} / {1}", MediaPlayer.Position.ToString(@"mm\:ss"), MediaPlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
-            }
+            if (MediaPlayer.Source != null && MediaPlayer.NaturalDuration.HasTimeSpan)
+                lblTimer.Content = String.Format("{0} / {1}", MediaPlayer.Position.ToString(@"mm\:ss"), MediaPlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
         }
 
         private void MediaPlayer_MediaFailed(object sender, ExceptionRoutedEventArgs e)
